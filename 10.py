a30880d947a79dcd2f7fcacaca68c2a381678077
woutboat <g1650582@mvrht.com>
2016-08-30 21:40:15 -0600

    fix 10 from source code

--- 10.py
+++ 10.py
@@ -1,20 +1,17 @@
-import time
+from itertools import takewhile
 
-with open('primes.txt', 'r') as myfile:
-    data=myfile.read().replace('\n', '')
+def eratosthenes():
+    '''Yields the sequence of prime numbers via the Sieve of Eratosthenes.'''
+    D = {}  # map composite integers to primes witnessing their compositeness
+    q = 2   # first integer to test for primality
+    while 1:
+        if q not in D:
+            yield q        # not marked composite, must be prime
+            D[q*q] = [q]   # first multiple of q not already marked
+        else:
+            for p in D[q]: # move each witness to its next multiple
+                D.setdefault(p+q,[]).append(p)
+            del D[q]       # no longer need D[q], free memory
+        q += 1
 
-newData = data.split(' ')
-
-su = 0
-
-for i in range(0, len(newData)):
-    if newData[i] == '':
-        newData[i] = 0
-
-print(newData)
-
-for x in range(0, len(newData)):
-    su = su + int(newData[x])
-    print(su)
-
-print(su)
+print(sum(takewhile(lambda x: x < 2000000, eratosthenes())))
