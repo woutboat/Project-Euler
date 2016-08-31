6d302212f2c6789c085c14efb98434920fd9e7b4
woutboat <g1650582@mvrht.com>
2016-08-30 21:36:23 -0600

    fixed 5

--- 5.py
+++ 5.py
@@ -1,15 +1,25 @@
 #Find the smallest number evenly divisible by all numbers from 1-20
 
-potat = True
-bu = 2520
-while potat:
-	print(bu)
-	tomat = True
-	for i in range(1,21):
-		if not bu % i == 0:
-			tomat = False
-	if tomat:
-		break
-	else:
-		bu+=1
-print bu
+def is_divisible_to(number, x):
+    for i in reversed(list(range(1, x+1))):
+        if number % i != 0:
+            return False
+    return True
+
+def divisible_to(x):
+    if x < 1:
+        return False
+    elif x == 1:
+        return 1
+    else:
+        step = divisible_to(x-1)
+        number = 0
+        found = False
+        while not found:
+            number += step
+            found = is_divisible_to(number, x)
+        return number
+
+print(divisible_to(20))
+
+#answer 232792560
