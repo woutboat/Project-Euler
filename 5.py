59b847c9cae8b51b1f213a479b1f9dc228977a1b
SirPigles <woutbouckaert@gmail.com>
2016-08-30 19:18:30 -0600

    Work on 5
    un tested

--- 5.py
+++ 5.py
@@ -1,7 +1,15 @@
 #Find the smallest number evenly divisible by all numbers from 1-20
 
 potat = True
-bu = 2521
+bu = 2
 while potat:
-    for i in range(1,21):
+	print(bu)
+	tomat = True
+    for i in range(1,11):
         if not bu % i == 0:
+			tomat = False
+	if tomat:
+		break
+	else:
+		bu+=1
+print bu
