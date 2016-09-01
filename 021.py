365fb38005dc523e18bbbcd4af37cd1f0fba3dc1
woutboat <g1650582@mvrht.com>
2016-08-31 21:17:20 -0600

    ready to run 21

---
+++ 021.py
@@ -0,0 +1,29 @@
+def divisors(n):
+	potat = []
+	for i in range(1, n):
+		if n % i == 0:
+			potat.append(i)
+	return potat
+
+def arrSum(arr):
+	su = 0
+	for i in range(0,len(arr)):
+		su += arr[i]
+	return su
+
+def a(n):
+	return arrSum(divisors(n))
+
+def b(n):
+	return arrSum(divisors(n))
+
+ehh = []
+
+for o in range(1,10001):
+	print(o)
+	for p in range(1,10001):
+		if a(o) == b(p):
+			ehh.append(o)
+			ehh.append(p)
+
+print(arrSum(ehh))
