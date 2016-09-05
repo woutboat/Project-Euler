20a4c5083d81253c1f58495303b4713b565b709d
woutboat <g1650582@mvrht.com>
2016-09-04 21:31:18 -0600

    fixing error

--- 052.py
+++ 052.py
@@ -17,9 +17,11 @@ while potato:
 	for i in range(2,7):
 		if sum_digits(num) == sum_digits(testMult(num, i)):
 			counter += 1
+	#print(str(num) + str(counter))
 	if counter == 6:
 		potato = False
 	else:
 		num += 1
+		counter = 0
 
 print(num)
